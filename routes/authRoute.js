3db696f60ddc2e4af367b9bb4af0ad279e02b404
Rohan Mali <rohanmali.2002@gmail.com>
2023-07-06 18:00:33 +0530

    starting with react frontend

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,18 @@
 import express from "express";
-
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 const router = express.Router();
+
+router.post("/register", registerController);
+
+//LOGIN ||post
+router.post("/login", loginController);
+
+//test Routes
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;
