2efd298d17d6d1a55cadb933f5905e5a78d82a37
Rohan Mali <rohanmali.2002@gmail.com>
2023-07-22 13:11:43 +0530

    Created CRUD operation backend for products

---
+++ routes/productRoutes.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
+const router = express.Router();
+
+//routes
+
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//update prod
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+//get products
+router.get("/get-products", getProductController);
+
+// singlle products get
+router.get("/get-products/:slug", getSingleProductController);
+
+//getPhoto
+router.get("/product-photo/:pid", productPhotoController);
+
+// delete Product
+router.delete("/product/:pid", deleteProductController);
+
+export default router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/CategoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 // env config
@@ -23,7 +24,7 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-
+app.use("/api/v1/products", productRoutes);
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Quickbuy</h1>");
